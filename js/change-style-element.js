d48c6f159bc984eb0aa133085bf887b177321bfb
Taha <taha.sebai@etu.sorbonne-universite.fr>
2025-03-14 14:47:55 +0000

    comments added to change-style-element

--- js/change-style-element.js
+++ js/change-style-element.js
@@ -1,15 +1,19 @@
 var selectElem = document.getElementById("selectElem");
 
+/* I select the articles, the sections and the body separately.
+Indeed, document.querySelectorAll("body", "article", "section") will only select the body.
+That's because body is the first element encountered. */
 const articles = document.querySelectorAll("article");
 const sections = document.querySelectorAll("section");
 const body = document.querySelectorAll("body");
-
 let elements = [...body, ...articles, ...sections];
 
 let previousElems = []; 
 
+// Add options in the element selector menu
 function addElementSelector(){
 
+    // Some elements have the same class, we don't want them to appear twice in the menu
     function alreadyOption(value){
         let repeated = selectElem.querySelectorAll("option");
         for (let i in repeated){
@@ -21,6 +25,7 @@ function addElementSelector(){
         return false;
     }
 
+    // add an option to the menu ONLY if the option doesn't already exist (e.g: 2 section with the same class)
     function createOption(text, value){
         if (alreadyOption(text) === false){
             var text = document.createTextNode(text);
@@ -31,9 +36,14 @@ function addElementSelector(){
         }
     }
 
+    /* we go through all the elements that we are interessted in our document
+        (article, section and body) and we add them in the menu */
     for (let i in elements){   
         let elem = elements[i];
 
+        /* the value of the the option is the name of the tag
+            but the content that will appear has to be either the id, the class or
+            the name of the element by default. That's what these conditions are for (more user friendly) */
         if(elem.id){
             createOption(elem.id, elem.tagName);
         }
@@ -49,9 +59,12 @@ function addElementSelector(){
 addElementSelector();
 
 
+
 let selectStyle = document.getElementById("selectStyle");
 selectElem.addEventListener("change", changeStyle, false);
 
+/* function to reset the style of the webpage.
+    Otherwise, the styles will continue to be added to eachother */
 function resetPreviousStyles() {
     for (let e of previousElems) {
         e.style.fontSize = ""; 
@@ -60,7 +73,14 @@ function resetPreviousStyles() {
     previousElems = []; 
 }
 
+
+/* Change the style of one or multiple elements.
+    If the user chooses an option that corresponds to several elements (like article-container) */
 function changeStyle(event){
+    /*
+        selectedElem: value of the option selected, i.e: name of the element that the user want to change the appearance
+        selectedOption: id or class of the selected element. If it doesn't have any of these attribute, selectedOption is just the name of the element (like body)
+    */
     let selectedElem = selectElem.value;
     let selectedOption = event.target.selectedOptions[0].textContent;
 
