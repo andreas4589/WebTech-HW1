9e36372590c2af1a3c2bc948be27eeca4efcd4b9
PMM <maxmaes2004@gmail.com>
2025-04-03 18:10:24 +0200

    We can now add courses and users. I also added EJS but could not yet get it to work with showing the list of courses correctly.

--- main.js
+++ main.js
@@ -4,21 +4,43 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-
+const {registerUser, getUsers, getCourses} = require('./database');
 const PORT = 8038;
 const HOST = 'localhost';
 
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+const testPerson = {firstName: "Max", lastName: "Maes", password: "", email: "bla", major: "", age: ""}
+console.log(registerUser(testPerson));
+
+app.post('/register', (req, res) => {
+  const {firstName, lastName, password, email, major, age} = req.body;
+  registerUser({firstName, lastName, password, email, major, age});
+})
+
+app.get('/register', (req, res) => {
+ getCourses((err, result) => {
+   res.render('register', {coursesList: result});
+ })
+})
+app.get('/users', (req, res) => {
+  getUsers((err, users) => {
+    res.json(users);
+  });
 });
 
-app.get('/style.css', function(req, res) {
-  res.sendFile(__dirname + "/public/style.css");
+app.get('/courses', (req, res) => {
+  getCourses((err, courses) => {
+    res.json(courses);
+  });
 });
-
-
-
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:' + PORT );
 });
