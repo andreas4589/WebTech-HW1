81f8239d18045a5fb9558d7d920056b1d04ee1a3
Taha <taha.sebai@etu.sorbonne-universite.fr>
2025-04-05 01:30:09 +0000

    inbox and message implemented

--- users.js
+++ users.js
@@ -32,4 +32,106 @@ router.get("/profile/:userid", function (req, res) {
 
 
 
+
+
+
+
+/*
+added by Taha 
+I assumed here that in the table message, the row sender and recipient
+corespond to the email adresses
+*/
+router.get("/message-box/:userid", function (req, res) {
+    if(req.session.user){
+        var name;
+        db.get("SELECT firstName FROM users WHERE email = ?", [req.session.user], function (err, result) {
+            if (result !== undefined) {
+                name=result.name;
+                db.all(`
+                    SELECT u.firstName, u.lastName, u.email, m.message FROM messages m, users u
+                    WHERE m.recipient = ? AND u.email=m.recipient`, [name],
+                    function (err, result) {
+                        if (result !== undefined) {
+                            res.render("message-box", {name: name, messages_list: result});
+                        }
+                        else{
+                            res.render("message-box", {name: name, messages_list: []});
+                        }                        
+                });
+            }
+            else {res.send("Not Found");}
+        });
+    }
+    else{res.redirect("/");}
+})
+
+
+router.get("/write-message/:recipient", function (req, res) {
+    var recipient = req.params.recipient;
+    if(req.session.user){
+        var name;
+        db.get("SELECT firstName FROM users WHERE email = ?", [req.session.user], function (err, result) {
+            if (result !== undefined) {
+                name=result.name;
+                db.all(`
+                    SELECT u.firstName, u.lastName, f.friend_email FROM users u friends f
+                    WHERE f.user_email=? AND f.friend_email=u.email`, [req.session.userid],
+                    function (err, result) {
+                        if (result !== undefined) {
+                            res.render("write-message", {name: name, friends_list: result, recipient: recipient});
+                        }
+                        else{
+                            res.render("write-message", {name: name, friends_list: [], recipient: recipient});
+                        }  
+                });
+            }
+            else {res.send("Not Found");}
+        });
+    }
+    else{res.redirect("/");}
+})
+
+router.get("/write-message", function (req, res) {
+    var recipient = "";
+    if(req.session.user){
+        var name;
+        db.get("SELECT firstName FROM users WHERE email = ?", [req.session.user], function (err, result) {
+            if (result !== undefined) {
+                name=result.name;
+                db.all(`
+                    SELECT u.firstName, u.lastName, f.friend_email FROM users u friends f
+                    WHERE f.user_email=? AND f.friend_email=u.email`, [req.session.userid],
+                    function (err, result) {
+                        if (result !== undefined) {
+                            res.render("write-message", {name: name, friends_list: result, recipient: recipient});
+                        }
+                        else{
+                            res.render("write-message", {name: name, friends_list: [], recipient: recipient});
+                        }  
+                });
+            }
+            else {res.send("Not Found");}
+        });
+    }
+    else{res.redirect("/");}
+})
+
+router.post("/sendMessage", function (req, res) {
+    if(req.session.user){
+        var sender = req.session.userid;
+        var recipient = req.body.recipient;
+        var message = req.body.message-content;
+
+        db.run('INSERT INTO messages (sender, recipient, message) VALUES (?, ?, ?)', [sender, recipient, message]);
+
+        res.send("The message was sent successfully !");
+    }
+    else{
+        res.redirect("/login");
+    }
+})
+
+
+
+
 module.exports = router;
